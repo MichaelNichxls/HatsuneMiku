4757b90cca1c67f6f815ff0ea707fdd774383d03
Michael Nichols <mjnichols10@yahoo.com>
2022-12-23 23:23:22 -0600

    Added bonk command and its slash command.

---
+++ src/HatsuneMiku/Commands/BonkCommand.cs
@@ -0,0 +1,32 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace HatsuneMiku.Commands
+{
+    //[Category]
+    [RequireGuild]
+    public class BonkCommand : BaseCommandModule
+    {
+        // Make configurable
+        public static DateTimeOffset TimeoutUntil => DateTimeOffset.UtcNow.AddSeconds(10);
+
+        // Relocate link
+        // Disallow timeout if member in question is in vc
+        [Command("bonk")]
+        [Description("Bonks the horny")]
+        public async Task Bonk(CommandContext ctx, DiscordMember member)
+        {
+            try
+            {
+                await member.TimeoutAsync(TimeoutUntil, "Horny").ConfigureAwait(false);
+            }
+            finally
+            {
+                await ctx.Channel.SendMessageAsync($"Bonk, {member.Mention}", new DiscordEmbedBuilder().WithImageUrl("https://i.imgur.com/w2rfJnr.png")).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ src/HatsuneMiku/Commands/SlashCommands/BonkSlashCommand.cs
@@ -0,0 +1,32 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
+using System;
+using System.Threading.Tasks;
+
+namespace HatsuneMiku.Commands.SlashCommands
+{
+    // Relocate?
+    //[SlashCommandGroup]
+    [SlashRequireGuild]
+    public class BonkSlashCommand : ApplicationCommandModule
+    {
+        public static DateTimeOffset TimeoutUntil => BonkCommand.TimeoutUntil;
+
+        [SlashCommand("bonk", "Bonks the horny")]
+        //[SlashCommandPermissions]
+        public async Task Bonk(InteractionContext ctx, [Option("user", "User to bonk")] DiscordUser user)
+        {
+            DiscordMember member = await ctx.Guild.GetMemberAsync(user.Id).ConfigureAwait(false);
+
+            try
+            {
+                await member.TimeoutAsync(TimeoutUntil, "Horny").ConfigureAwait(false);
+            }
+            finally
+            {
+                await ctx.CreateResponseAsync($"Bonk, {member.Mention}", new DiscordEmbedBuilder().WithImageUrl("https://i.imgur.com/w2rfJnr.png")).ConfigureAwait(false);
+            }
+        }
+    }
+}
