1d7982ca6a145144176ee9d30bbfc6cd7648265f
Michael Nichols <mjnichols10@yahoo.com>
2023-01-03 21:43:24 -0600

    Added Hatsune Miku GIF and image commands.

---
+++ src/HatsuneMiku/Commands/MikuGifCommand.cs
@@ -0,0 +1,22 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HatsuneMiku.Commands;
+
+public class MikuGifCommand : BaseCommandModule
+{
+    private readonly IEnumerable<string> _gifs;
+
+    public MikuGifCommand(ImageUrlServiceResolver images) =>
+        _gifs = images(ImageType.Animated);
+
+    [Command("mikugif"), Aliases("mikugiphy", "mikutenor")]
+    [Description("")]
+    public async Task MikuGif(CommandContext ctx) =>
+        await ctx.Channel.SendMessageAsync(new DiscordEmbedBuilder().WithImageUrl(_gifs.ElementAt(new Random().Next(_gifs.Count())))).ConfigureAwait(false);
+}
---
+++ src/HatsuneMiku/Commands/MikuImageCommand.cs
@@ -0,0 +1,22 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HatsuneMiku.Commands;
+
+public class MikuImageCommand : BaseCommandModule
+{
+    private readonly IEnumerable<string> _images;
+
+    public MikuImageCommand(ImageUrlServiceResolver images) =>
+        _images = images(ImageType.Photo);
+
+    [Command("mikuimage"), Aliases("mikuimg")]
+    [Description("")]
+    public async Task MikuImage(CommandContext ctx) =>
+        await ctx.Channel.SendMessageAsync(new DiscordEmbedBuilder().WithImageUrl(_images.ElementAt(new Random().Next(_images.Count())))).ConfigureAwait(false);
+}
